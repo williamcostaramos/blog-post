9a6ab884193d20829aec957107fa987fec291f6b
ramos.william <ramos.william@mj.gov.br>
2022-06-29 07:33:28 -0300

    first commit project

---
+++ src/test/java/com/william/blogpost/BlogPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.blogpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
